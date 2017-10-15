0027f0d563a30f0017934cbdf4a64508b95d1f96
hodge <hodge@DESKTOP-UT7VA9R>
2017-10-15 17:56:31 -0500

    added set stubs to binarysearchtree

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,2 +1,44 @@
 class BinarySearchTree<T> implements Set<T> {
+
+	@Override
+	public void add(T obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(T obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void remove(T obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public T[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
