84a6ab1cb814c53c4bebd0384d2b2fde8637a323
hodge <hodge@172.16.2.94>
2017-10-25 14:02:47 -0500

    added tests for contains, isEmpty, and remove BST

--- src/BSTtester.java
+++ src/BSTtester.java
@@ -6,6 +6,9 @@ class BSTtester{
 		boolean passed = testAdd();
         passed &= testConstructor();
         passed &= testClear();
+        passed &= testContains();
+        passed &= testIsEmpty();
+        passed &= testRemove();
         if (passed) {
             System.out.println("All tests passed");
         }
@@ -26,8 +29,8 @@ class BSTtester{
 	
 	public static boolean testAdd(){
 		BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>();
-		int[] data = {7, 3, 19, 4, 5, 4, 100, 6, 17};
-		for (int x: data) {
+		Integer[] data = {7, 3, 19, 4, 5, 4, 100, 6, 17};
+		for (Integer x: data) {
             tree.add(x);
         }
 		 return testMethod("add test", tree.toString().equals("[3, 4, 5, 6, 7, 17, 19, 100]"));
@@ -44,7 +47,31 @@ class BSTtester{
 		BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>(lst);
 		tree.clear();
 		boolean passed = testMethod("add test", tree.toString().equals("[]"));
-		return passed && tree.size() == 0;
+		return testMethod("clear test", tree.size() == 0) && passed;
+	}
+	
+	public static boolean testContains(){
+		Integer[] lst = {7, 3, 19, 4, 5, 4, 100, 6, 17};
+		BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>(lst);
+		return testMethod("contains test", tree.contains(7) && !tree.contains(94) && tree.contains(4));
+	}
+	
+	public static boolean testIsEmpty(){
+		Integer[] lst = {7, 3, 19, 4, 5, 4, 100, 6, 17};
+		BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>(lst);
+		boolean passed = !tree.isEmpty();
+		tree.clear();
+		return testMethod("isEmpty test", passed && tree.isEmpty());
+	}
+	
+	public static boolean testRemove(){
+		Integer[] lst = {7, 3, 19, 4, 5, 4, 100, 6, 17};
+		BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>();
+		boolean passed = testMethod("remove test 1", !tree.remove(5));
+		for (Integer x: lst) {
+            tree.add(x);
+        }
+		return passed & testMethod("remove test 2", tree.remove(5) && tree.size() == 7);
 	}
 		
 }
